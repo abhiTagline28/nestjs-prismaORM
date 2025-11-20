bd83dfb8603108518882629e25e90b5425023e23
Abhimanyu <ams@taglineinfotech.com>
2025-11-20 12:23:17 +0530

    Added login api

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+export const jwtSecret = 'zjP9h6ZI5LoSKCRj';
+
+@Module({
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: '10m' }, // e.g. 30s, 7d, 24h
+      // signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
